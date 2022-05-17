b998016802e30fceb692f232817945bc2831c5c5
Jonathan Hockman <jonathan.hockman@gmail.com>
2022-05-17 10:30:45 -0300

    update tests

--- sumarr/test/counter_test.exs
+++ sumarr/test/counter_test.exs
@@ -1,21 +1,44 @@
-defmodule Sumarr.CounterTest  do
-    use ExUnit.Case
-    import Sumarr.Counter
-    describe "new/1" do
-        test "outputs a map with %{counter: integer}" do
-            assert %{counter: 42} = new("42")
-        end
-    end
+defmodule Sumarr.CounterTest do
+  use ExUnit.Case
+  import Sumarr.Counter
 
-    describe "add/2" do
-        test "adds second argument to current counter value" do
-            assert %{counter: 43} = add(%{counter: 42}, 1)
-        end
+  describe "new/1" do
+    test "outputs a map with %{counter: integer}" do
+      assert %{counter: 42} = new("42")
     end
+  end
 
-    describe "message/1" do
-        test "reports correct message" do
-            assert "The result is 43." = message(%{counter: 43})
-        end
+  describe "add/2" do
+    test "adds second argument to current counter value" do
+      assert %{counter: 43} = add(%{counter: 42}, 1)
     end
+  end
+
+  describe "message/1" do
+    test "reports correct message" do
+      assert "The result is 43." = message(%{counter: 43})
+    end
+  end
+
+  test "full CRC flow" do
+    actual =
+      "41"
+      |> new()
+      |> assert_count(41)
+      |> add(1)
+      |> assert_count(42)
+      |> add(1)
+      |> assert_count(43)
+      |> add(-1)
+      |> assert_count(42)
+      |> message()
+
+    expected = "The result is 42."
+    assert actual === expected
+  end
+
+  defp assert_count(counter, expected) do
+    assert counter.counter == expected
+    counter
+  end
 end
